8515aa7c961095d13f86165c52c902bd14207e2c
jkeum-dev <jon9eun@gmail.com>
2023-11-22 11:40:40 +0900

    2-05 Repository - 데이터 삭제하기

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -81,4 +81,14 @@ class SbbApplicationTests {
 		this.questionRepository.save(q);
 	}
 
+	@Test
+	void testJpa8() {
+		assertEquals(2, this.questionRepository.count());
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		this.questionRepository.delete(q);
+		assertEquals(1, this.questionRepository.count());
+	}
+
 }
